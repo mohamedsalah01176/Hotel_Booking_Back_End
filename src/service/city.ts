0a1621aafac9ab21a521c536ca680420664ae4d2
mohamed salah <m48162698@gmial.com>
2025-07-22 19:21:30 +0300

    handle all cities

--- src/service/city.ts
+++ src/service/city.ts
@@ -1,3 +1,25 @@
+import CityModel from "../model/city"
+
 export default class CityService{
-  
+  async handleGetCities(){
+    try{
+      const cities=await CityModel.find({});
+      if(cities.length>0){
+        return{
+          status:"succes",
+          cities
+        }
+      }else{
+        return{
+          status:"fail",
+          message:"Not Found Any City"
+        }
+      }
+    }catch(errors){
+      return{
+        status:"error",
+        errors
+      }
+    }
+  }
 }
