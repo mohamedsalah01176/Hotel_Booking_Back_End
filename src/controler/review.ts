81e8cfc6d31bf00819751eb0c1eb687247f48638
mohamed salah <m48162698@gmial.com>
2025-07-24 17:41:40 +0300

    modify on property model and handle add comment

---
+++ src/controler/review.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import ReviewServices from "../service/review";
+import { ReponseStatues } from "../util/ResponseStatus";
+import { IUserBody } from "../interface/user";
+
+export default class ReviewController{
+  constructor(private reviewServices:ReviewServices){}
+
+  async addReview(req:Request,res:Response){
+    const body=req.body;
+    const lang=req.query.lang as string;
+    const propertyId=req.params.propertyId
+    const user=req.user as IUserBody
+    let responseServer=await this.reviewServices.handleAddReview(body,user,propertyId,lang);
+    ReponseStatues(responseServer,res)
+  }
+}
