1d4c73e97339950fdc2403b34a4c12d18084d9ed
mohamed salah <m48162698@gmial.com>
2025-08-12 10:49:07 +0300

    handle get specific user and update user

--- src/route/user.ts
+++ src/route/user.ts
@@ -1,6 +1,8 @@
 import {  Router } from "express";
 import UserService from "../service/user";
 import UserControler from "../controler/user";
+import { authentication } from "../meddileware/user";
+import upload from "../meddileware/cloudnary";
 
 const router=Router();
 
@@ -14,6 +16,8 @@ router.post("/register",(req,res)=>userControler.register(req,res))
 router.post("/login",(req,res)=>userControler.login(req,res))
 router.post("/forgetPassword",(req,res)=>userControler.forgetPassword(req,res))
 router.post("/resetPassword",(req,res)=>userControler.resetPassword(req,res))
+router.get("/setting",authentication,(req,res)=>userControler.getSpecificUser(req,res))
+router.patch("/setting",authentication,upload.single("image"),(req,res)=>userControler.updateUser(req,res))
 
 
 
